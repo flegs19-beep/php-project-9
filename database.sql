b65e05361c2b7d0f94c6d5d5b9ec2ea2a23682ba
flegs19-beep <flegs19@gmail.com>
2026-09-07 21:27:48 +0300

    Allow long SEO values

--- database.sql
+++ database.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id bigint NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
     status_code integer,
-    h1 varchar(255),
-    title varchar(255),
-    description varchar(255),
+    h1 text,
+    title text,
+    description text,
     created_at timestamp NOT NULL
 );
